b2ab324dcdb90b988cb28c532bd087ce8fc5d980
kumartul <kumartul001@gmail.com>
2022-06-01 09:41:07 +0530

    [Refactor]: Remove unused parameter 'next'

--- routes/null.js
+++ routes/null.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
   res.status(404).render("error");
 });
 
