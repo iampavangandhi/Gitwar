ce56ecfd843a25841e5c3b0ab998baef21556466
Nathan Hedge <23344786+10Nates@users.noreply.github.com>
2023-04-05 19:38:03 -0500

    fixed syntax errors, fixed opaque error messages, fixed crashes from invalid user input

--- utils/calc.js
+++ utils/calc.js
@@ -5,48 +5,49 @@ axios.defaults.headers.common['Authorization'] = "Bearer " + process.env.GITHUB_
 
 const getProfile = async (username = "iampavangandhi") => {
   try {
-    const { data } = await axios.get(
+    const user = await axios.get(
       `https://api.github.com/users/${username}`
     );
+    const data = user?.data;
     const score = await calculateScore(data);
-    const user_orgs = await userOrgs(data?.login);
+    const user_orgs = await userOrgs(data.login);
     const { totalRepoStars: repo_stars, totalRepoForks: repo_forks } =
-      await getRepoStarsAndForks(data?.login);
+      await getRepoStarsAndForks(data.login);
 
     let myProfile = {
-      avatar: data?.avatar_url,
-      username: data?.login,
-      name: data?.name,
-      public_repos: data?.public_repos,
+      avatar: data.avatar_url,
+      username: data.login,
+      name: data.name,
+      public_repos: data.public_repos,
       repo_stars: repo_stars,
       repo_forks: repo_forks,
-      followers: data?.followers,
+      followers: data.followers,
       user_orgs: user_orgs,
       score: score,
-      url: data?.html_url,
+      url: data.html_url,
     };
     return myProfile;
   } catch (error) {
-    console.log("Error in getProfile");
-    throw new Error(error);
+    console.error(error.name + ": " + error.message); // short as it is common (user input based)
+    return defaultProfile; // fallback
   }
 };
 
 // Calculate Profile Score
 const calculateScore = async (profile) => {
   try {
-    const star = await staredRepos(profile?.login);
-    const org = await userOrgs(profile?.login);
+    const star = await staredRepos(profile.login);
+    const org = await userOrgs(profile.login);
     const { totalRepoStars: repo_stars, totalRepoForks: repo_forks } =
-      await getRepoStarsAndForks(data?.login);
+      await getRepoStarsAndForks(profile.login);
 
     const orgs = Number(org) * 50;
     const stars = Number(star) * 5;
     const repoStarsScore = Number(repo_stars) * 5;
     const repoForksScore = Number(repo_forks) * 5;
-    const followers = Number(profile?.followers) * 15;
-    const publicRepos = Number(profile?.public_repos) * 10;
-    const publicGists = Number(profile?.public_gists) * 5;
+    const followers = Number(profile.followers) * 15;
+    const publicRepos = Number(profile.public_repos) * 10;
+    const publicGists = Number(profile.public_gists) * 5;
 
     const score =
       stars +
@@ -59,8 +60,8 @@ const calculateScore = async (profile) => {
 
     return score;
   } catch (error) {
-    console.log("Error in calculateScore");
-    throw new Error(error);
+    console.error(error); // this should be more detailed as it should be infrequent
+    return 0; // fallback
   }
 };
 
@@ -72,8 +73,8 @@ const userOrgs = async (profile) => {
     );
     return orgs?.data?.length;
   } catch (error) {
-    console.log("Error in userOrgs");
-    throw new Error(error);
+    console.error(error.name + ": " + error.message); // short as it is common (user input based)
+    return 0; // fallback
   }
 };
 
@@ -86,8 +87,8 @@ const staredRepos = async (profile) => {
 
     return star.data?.length;
   } catch (error) {
-    console.log("Error in staredRepos");
-    throw new Error(error);
+    console.error(error.name + ": " + error.message); // short as it is common (user input based)
+    return 0; // fallback
   }
 };
 
@@ -99,14 +100,14 @@ const getRepoStarsAndForks = async (profile) => {
     const userRepos = await axios.get(
       `https://api.github.com/users/${profile}/repos?per_page=500&type=all`
     );
-    userRepos?.forEach((repo) => {
-      totalRepoStars += Number(repo?.stargazers_count);
-      totalRepoForks += Number(repo?.forks_count);
+    userRepos?.data?.forEach((repo) => {
+      totalRepoStars += Number(repo.stargazers_count);
+      totalRepoForks += Number(repo.forks_count);
     });
     return { totalRepoStars, totalRepoForks };
   } catch (error) {
-    console.log("Error in getRepoStarsAndForks");
-    throw new Error(error);
+    console.error(error.name + ": " + error.message); // short as it is common (user input based)
+    return { totalRepoStars: 0, totalRepoForks: 0 }; // fallback
   }
 };
 
